bbfc6f17b6a3d76a64b95ffb2e9418eda869c83c
Schnitzel <rw21062006@gmail.com>
2023-10-25 19:23:35 +0200

    add multithreading

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ import sys
 import pkg_resources
 import random
 import time
+import threading
 
 #make sure additional packages are installed
 required = {'urllib3', 'requests', 'beautifulsoup4', 'bs4', 'lxml'}
@@ -22,6 +23,106 @@ if missing:
 from bs4 import BeautifulSoup
 import urllib.request
 import requests
+
+def download_post():
+    global completed_downloads#
+    global repeats
+    #generating post index
+    if direction == "r":
+        post_index = total_posts.pop(random.randint(0, len(total_posts)-1))
+    elif direction == "n":
+        post_index = total_posts.pop(0)
+    else:
+        post_index = total_posts.pop(len(total_posts)-1)
+    #getting the closest multiple of 42 to the post index
+    closest_42_to_index = int((post_index-1) / 42) * 42
+
+    #making page_link go to the page the post_index post is on
+    page_link = "https://" + base_link + "/index.php?page=post&s=list&tags=" + tags + taglist + "+&pid=" + str(closest_42_to_index)
+
+    while True:
+        #getting html once again
+        source = requests.get(page_link, headers=headers).text
+        soup = BeautifulSoup(source, 'lxml')
+        #indexing all the post ids on the page
+        res = soup.findAll("span", {"class" : "thumb"})
+        id_list = []
+        for r in res:
+            id_list.append(str(r['id'])[1:])
+        if len(id_list) > 0:
+            break
+
+    #making link to post with id
+    post_id = id_list[post_index - closest_42_to_index - 1]
+    post_link = "https://" + base_link + "/index.php?page=post&s=view&id=" + post_id
+    
+    while True:
+        try:
+            #html, again
+            source = requests.get(post_link, headers=headers).text
+            soup = BeautifulSoup(source, 'lxml')
+            #getting link to the source image
+            res = soup.find("div", {"class":"link-list"}).contents[3].contents
+            break
+        except:
+            pass
+    k = 1
+    while True:
+        image_link = res[k].contents[1]["href"]
+        if image_link != "#":
+            break
+        k += 2
+
+    #getting file ending index
+    j = len(image_link) - image_link.rfind(".")
+
+    filename = f"{tags}/{tags} [{post_id}]{image_link[-j:image_link.rfind('?')]}"
+    #skipping if the file has already been downloaded previously
+    if os.path.exists(filename):
+        if stop_on_repeat:
+            repeats = completed_downloads
+            return
+        #if it is set to download everything, but one post has already been downloaded, decrease amount to download by 1
+        #otherwise pick another post instead (don't mark this as a succesful download by incrementing i)
+        if repeats - completed_downloads - 1 == len(total_posts):
+            repeats -= 1
+        return
+    
+    if not os.path.exists(tags):
+        os.mkdir(tags)
+
+    try:
+        #downloading the image/video
+        r = urllib.request.urlopen(image_link)
+        with open(filename, "wb") as file:
+            file.write(r.read())
+    #in case of 404 not found
+    except Exception:
+        #attempt to get link through img src
+        image_link = None
+        res = soup.find("img", {"id":"image"})
+        if res != None:
+            image_link = res['src']
+        else:
+            res = soup.find("video", {"id":"gelcomVideoPlayer"})
+            if res != None:
+                image_link = res.contents[1]['src']
+
+        if image_link != None:
+            j = len(image_link) - image_link.rfind(".")
+            filename = f"{tags}/{tags} [{post_id}]{image_link[-j:image_link.rfind('?')]}"
+            
+            r = urllib.request.urlopen(image_link)
+        else:
+            #if it is set to download everything, but one post can't be found, decrease amount to download by 1
+            if repeats - completed_downloads - 1 == len(total_posts):
+                repeats -= 1
+            return
+        
+        with open(filename, "wb") as file:
+            file.write(r.read())
+
+    completed_downloads += 1
 
 if len(sys.argv) > 1:
     print(f"Changing directory to {sys.argv[1]}")
@@ -89,96 +190,20 @@ total_posts = list(range(1, total_posts+1))
 
 tags = tags.replace("+", " ")
 inital_repeats_string_len = len(str(repeats))
+completed_downloads = 0
 i = 0
-while i < repeats:
-    percentage = f"{(float(i) / repeats * 100):.2f}%".rjust(7).ljust(10)
-    progress_string = f"Progress: {percentage}({str(i).rjust(inital_repeats_string_len)} / {str(repeats).rjust(inital_repeats_string_len)})"
-    print(progress_string, end="\r")
-    #generating post index
-    if direction == "r":
-        post_index = total_posts.pop(random.randint(0, len(total_posts)-1))
-    elif direction == "n":
-        post_index = total_posts.pop(0)
-    else:
-        post_index = total_posts.pop(len(total_posts)-1)
-    #getting the closest multiple of 42 to the post index
-    closest_42_to_index = int((post_index-1) / 42) * 42
-
-    #making page_link go to the page the post_index post is on
-    page_link = "https://" + base_link + "/index.php?page=post&s=list&tags=" + tags + taglist + "+&pid=" + str(closest_42_to_index)
-    #getting html once again
-    source = requests.get(page_link, headers=headers).text
-    soup = BeautifulSoup(source, 'lxml')
-    #indexing all the post ids on the page
-    res = soup.findAll("span", {"class" : "thumb"})
-    id_list = []
-    for r in res:
-        id_list.append(str(r['id'])[1:])
-
-    #making link to post with id
-    post_id = id_list[post_index - closest_42_to_index - 1]
-    post_link = "https://" + base_link + "/index.php?page=post&s=view&id=" + post_id
-    #html, again
-    source = requests.get(post_link, headers=headers).text
-    soup = BeautifulSoup(source, 'lxml')
-    #getting link to the source image
-    res = soup.find("div", {"class":"link-list"}).contents[3].contents
-    k = 1
-    while True:
-        image_link = res[k].contents[1]["href"]
-        if image_link != "#":
-            break
-        k += 2
-
-    #getting file ending index
-    j = len(image_link) - image_link.rfind(".")
-
-    filename = f"{tags}/{tags} [{post_id}]{image_link[-j:image_link.rfind('?')]}"
-    #skipping if the file has already been downloaded previously
-    if os.path.exists(filename):
-        if stop_on_repeat:
-            repeats = i
-            break
-        #if it is set to download everything, but one post has already been downloaded, decrease amount to download by 1
-        #otherwise pick another post instead (don't mark this as a succesful download by incrementing i)
-        if repeats - i - 1 == len(total_posts):
-            repeats -= 1
-        continue
-
-    try:
-        #downloading the image/video
-        r = urllib.request.urlopen(image_link)
-    #in case of 404 not found
-    except Exception:
-        #attempt to get link through img src
-        image_link = None
-        res = soup.find("img", {"id":"image"})
-        if res != None:
-            image_link = res['src']
-        else:
-            res = soup.find("video", {"id":"gelcomVideoPlayer"})
-            if res != None:
-                image_link = res.contents[1]['src']
-
-        if image_link != None:
-            j = len(image_link) - image_link.rfind(".")
-            filename = f"{tags}/{tags} [{post_id}]{image_link[-j:image_link.rfind('?')]}"
-            
-            r = urllib.request.urlopen(image_link)
-        else:
-            #if it is set to download everything, but one post can't be found, decrease amount to download by 1
-            if repeats - i - 1 == len(total_posts):
-                repeats -= 1
-            continue
-
-    if not os.path.exists(tags):
-        os.mkdir(tags)
-
-    with open(filename, "wb") as file:
-        file.write(r.read())
-
-    i += 1
+previous_percentage = ""
+while completed_downloads < repeats:
+    percentage = f"{(float(completed_downloads) / repeats * 100):.2f}%".rjust(7).ljust(10)
+    if percentage != previous_percentage:
+        previous_percentage = percentage
+        progress_string = f"Progress: {percentage}({str(completed_downloads).rjust(inital_repeats_string_len)} / {str(repeats).rjust(inital_repeats_string_len)})"
+        print(progress_string, end="\r")
+    time.sleep(0.1)
+    if i < repeats:
+        threading.Thread(target=download_post).start()
+        i += 1
 
 percentage = "100.00%   "
-progress_string = f"Progress: {percentage}({str(i).rjust(inital_repeats_string_len)} / {str(repeats).rjust(inital_repeats_string_len)})"
+progress_string = f"Progress: {percentage}({str(completed_downloads).rjust(inital_repeats_string_len)} / {str(repeats).rjust(inital_repeats_string_len)})"
 print(progress_string)
